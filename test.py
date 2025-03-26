c72c4e6413956cf8ffc811aa88eb6597eac6f291
Matthieu Segui <matthieu.segui@qorvo.com>
2025-03-26 01:10:34 +0100

    First try server

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import socket
 
 #Listens for incoming connections on port 3000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 3000))
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('', 3000))
 server.listen(5)
-print('Listening on localhost:3000')
+# print('Listening on localhost:3000')
 print('Waiting for connection')
-client, addr = server.accept()
-print(f'Connected to {addr}')
+while True:
+    client, addr = server.accept()
+    print(f'Connected to {addr}')
